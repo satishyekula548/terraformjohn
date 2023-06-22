a65a7ea3c7a0ccd23d9408da8ae5fa1be18b7925
satishyekula548 <130637410+satishyekula548@users.noreply.github.com>
2023-06-22 10:16:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my-instance" {
   ami                         = "ami-06ddbc40823fba89a"
   instance_type               = "t2.micro"
-  key_name                    = "uproject1"
+  key_name                    = "sattish"
   vpc_security_group_ids      = ["${aws_security_group.my-sg.id}"]
   subnet_id                   = "${aws_subnet.public-subnet-1.id}"
   associate_public_ip_address = true
@@ -14,7 +14,7 @@ resource "aws_instance" "my-instance" {
 resource "aws_instance" "my-instance1" {
   ami                         = "ami-06ddbc40823fba89a"
   instance_type               = "t2.micro"
-  key_name                    = "uproject1"
+  key_name                    = "sattish"
   vpc_security_group_ids      = ["${aws_security_group.my-sg.id}"]
   subnet_id                   = "${aws_subnet.public-subnet-2.id}"
   associate_public_ip_address = true
